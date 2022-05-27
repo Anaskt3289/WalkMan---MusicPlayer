123413f1f200d92059324741c880419f1a841bfa
anaskt <anaskt3289@gmail.com>
2022-05-27 18:57:42 +0530

    add songs server side almost completed

--- client/src/Components/AddSongsForm/AddSongs.jsx
+++ client/src/Components/AddSongsForm/AddSongs.jsx
@@ -1,56 +1,94 @@
 import React, { useState } from 'react'
-import { Form, Button} from 'react-bootstrap'
+import { Form, Button } from 'react-bootstrap'
+import axios from 'axios'
+import {ServerURL} from '../../Constants/Constants'
 
-import AddArtistModal from '../AddArtistModal/AddArtistModal' 
+import AddArtistModal from '../AddArtistModal/AddArtistModal'
 import './Styles/AddSongs.css'
 
 function AddSongs() {
 
+    const initialState = {
+        songName: '',
+        dateReleased: '',
+        artist: ''
+    }
+
     const [modalShow, setModalShow] = useState(false)
+    const [formData, setFormData] = useState(initialState)
+    const [artwork, setArtWork] = useState()
+    const [errMsg, setErrMsg] = useState()
+
+    const handleOnchange = (e) => {
+        const { name, value } = e.target
+        setFormData({ ...formData, [name]: value })
+    }
+    const config = {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      };
+
+    const AddSongSubmit = () => {
+        if (!(formData.songName && formData.dateReleased && formData.artist && artwork)) {
+            setErrMsg("Enter the required details to add the songs")
+            window.scrollTo(0, 0);
+        } else {
+            setErrMsg(null)
+            const data = new FormData()
+            data.append('artwork', artwork)
+            data.append('details', JSON.stringify(formData))
+
+            axios.post(`${ServerURL}/addSongs`, data, config).then((resp) => {
+               
+              }).catch((err) => {
+                console.log(err);
+              })
+
+        }
+    }
+
     return (
         <div>
             <div className='AddSongOuter'>
                 <h4>Add a New Song</h4>
+                <p className='errMsg'>{errMsg}</p>
                 <Form>
                     <Form.Group className="mb-3 formGroup" controlId="formBasicEmail">
                         <Form.Label className='formLabel'>Song Name</Form.Label>
-                        <Form.Control type="text" className='formTextBox' />
+                        <Form.Control type="text" name='songName' className='formTextBox' onChange={handleOnchange} />
                     </Form.Group>
                     <Form.Group className="mb-3 formGroup" controlId="formBasicEmail">
                         <Form.Label className='formLabel'>Date Realesed</Form.Label>
-                        <Form.Control type="text" className='formTextBox' />
+                        <Form.Control type="text" name='dateReleased' className='formTextBox' onChange={handleOnchange} />
                     </Form.Group>
                     <Form.Group className="mb-3 formGroup" controlId="formBasicEmail">
                         <Form.Label className='formLabel'>Artwork</Form.Label>
-                        <Form.Control type="file" className='formTextBox' />
-                    </Form.Group>
-                    <Form.Group className="mb-3 formGroup" controlId="formBasicEmail">
-                        <Form.Label className='formLabel'>Song</Form.Label>
-                        <Form.Control type="file" className='formTextBox' />
+                        <Form.Control type="file" className='formTextBox' onChange={(e) => setArtWork(e.target.files[0])} />
                     </Form.Group>
 
                     <Form.Group className="mb-3 formGroup" controlId="formBasicEmail">
                         <Form.Label className='formLabel'>Artist</Form.Label>
                         <div className='ArtistDetails'>
-                            <Form.Control list="browsers" name="browser" id="browser" />
-                            <datalist id="browsers">
+                            <Form.Control name='artist' list="artists" id="artists" onChange={handleOnchange} />
+                            <datalist id="artists">
                                 <option value="Edge" />
                                 <option value="Firefox" />
                                 <option value="Chrome" />
                                 <option value="Opera" />
                                 <option value="Safari" />
                             </datalist>
-                            <Button className='AddArtistBtn' variant="dark" onClick={()=>setModalShow(true)}>Add Artist</Button>
+                            <Button className='AddArtistBtn' variant="dark" onClick={() => setModalShow(true)}>Add Artist</Button>
                         </div>
                     </Form.Group>
                     <div className='formBtnsOuter'>
                         <Button variant="secondary" className='formBtns'>Cancel</Button>
-                        <Button variant="primary" className='formBtns'>Save</Button>
+                        <Button variant="primary" className='formBtns' onClick={AddSongSubmit}>Save</Button>
                     </div>
                 </Form>
             </div>
 
-            <AddArtistModal show={modalShow} onHide={()=>setModalShow(false)} />
+            <AddArtistModal show={modalShow} onHide={() => setModalShow(false)} />
         </div>
     )
 }
--- server(node js)/routes/users.js
+++ server(node js)/routes/users.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+const multer = require('multer')
+const fs = require('fs')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/temp-Storage')
+  },
+  filename: function (req, file, cb) {
+    cb(null, 'artwork.jpg')
+  }
+})
+
+var upload = multer({ storage: storage }).single('artwork')
+
+router.get('/', function (req, res, next) {
+  res.send('This is WalkMan Music-player Server');
+});
+
+router.post('/addSongs', function (req, res, next) {
+  upload(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      return res.status(500).json(err)
+    } else if (err) {
+      return res.status(500).json(err)
+    } else {
+      console.table(JSON.parse(req.body.details))
+      let songDetails = JSON.parse(req.body.details)
+    }
+  })
 });
 
 module.exports = router;
