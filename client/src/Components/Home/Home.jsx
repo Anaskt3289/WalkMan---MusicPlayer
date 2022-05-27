ef44f22df19cc097d0434b6e7be62a9624b2d71c
anaskt <anaskt3289@gmail.com>
2022-05-27 12:30:58 +0530

    home page ui completed

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,11 +1,43 @@
 import React from 'react'
-
+import { Button, Table } from 'react-bootstrap'
 import './Styles/Home.css'
 
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <div className='top10Songs'>
+        <div className='top10SongsTop'>
+          <h4 className='top10songsHeading'>Top 10 Songs</h4>
+          <Button variant="dark" className='AddSongsBtn'>ADD SONGS</Button>
+        </div>
+        <Table className='top10SongsTable'>
+          <thead>
+            <tr>
+              <th>Artwork</th>
+              <th>Song</th>
+              <th>Date of Release</th>
+              <th>Artist</th>
+              <th>Rate</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td> <img src="\Images\airpods pro-3.jpg" alt="" className='musicImage' /></td>
+              <td>Someone You Loved</td>
+              <td>July 29 2020</td>
+              <td>Lewis Capaldi</td>
+              <td>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+              </td>
+            </tr>
+
+          </tbody>
+        </Table>
+      </div>
     </div>
   )
 }
